59cff0ff6acef6c866297d12e137c7af11c474d6
Daniel Carlson <dancarl758@gmail.com>
2016-07-31 14:42:29 +0100

    adding footer and header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@
  */
 angular
   .module('ssicRApp', ['ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch', 'ui.router'])
-  .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('/', {
         url: '/home',
@@ -20,6 +20,36 @@ angular
       .state('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
+      })
+      .state('/teachers', {
+        url: '/teachers',
+        templateUrl: 'views/teachers.html',
+        controller: 'teachCtrl'
+      })
+      .state('/students', {
+        url: '/students', 
+        templateUrl: 'views/students.html',
+        controller: 'studentCtrl'
+      })
+      .state('/campus', {
+        url: '/campus',
+        templateUrl: 'views/campus.html', 
+        controller: 'campusCtrl'
+      })
+      .state('/support', {
+        url: '/support',
+        templateUrl: 'views/support.html', 
+        controller: 'supportCtrl'
+      })
+      .state('/admin', {
+        url: '/admin',
+        templateUrl: 'views/admin.html',
+        controller: 'adminCtrl'
+      })
+      .state('/contact', {
+        url: '/contact', 
+        templateUrl: 'views/contact.html',
+        controller: 'contactCtrl'
       })
       $urlRouterProvider.otherwise('/home');
   });
